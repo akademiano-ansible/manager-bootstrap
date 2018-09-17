6c18afcb882aa71703db98de5bc5306d04818e6f
mrdatamapper <mrdatamapper@gmail.com>
2018-09-17 20:00:54 +0000

    update to use with ansible playbooks

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,19 +16,19 @@ apt-get install ansible wget ca-certificates
 update-ca-certificates
 
 #download bootstrap playbook
-wget -O ansible-bootstrap.yml http://
+wget -O ansible-bootstrap.yml https://raw.githubusercontent.com/akademiano-ansible/linux-bootstrap/master/ansible-bootstrap.yml
 
 ansible-playbook --connection=local 127.0.0.1 ansible-bootstrap.yml
 
-wget -O ansible-roles-preload.yml http://
+wget -O ansible-roles-preload.yml https://raw.githubusercontent.com/akademiano-ansible/linux-bootstrap/master/ansible-roles-preload.yml
 
 ansible-playbook --connection=local 127.0.0.1 ansible-roles-preload.yml
 
 #if not exist param with default roles-install playbook url - use akademiano-full
 
-ROLES_URL=
+ROLES_URL=https://gist.githubusercontent.com/mrdatamapper/75fbd48ab24c7e4509cc1ca6122a1d0d/raw/a84c025dd08f739371d1758f2c0994294e161661/akademiano-ansible-roles-all.yml
 
-wget -O bootstrap-roles.yml http://
+wget -O bootstrap-roles.yml $ROLES_URL
 
 ansible-playbook --connection=local 127.0.0.1 bootstrap-roles.yml
 
