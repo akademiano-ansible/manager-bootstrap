696e9a13dc7564cc031abaf98a2c8e11eb0950d6
mrdatamapper <mrdatamapper@gmail.com>
2018-09-17 20:24:51 +0000

    change to use ansible playbooks, dd system dist-upgrade

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [[ $(id -u) -ne 0 ]]; 
+if [[ $(id -u) -ne 0 ]];
   then echo "Ubuntu dev bootstrapper, APT-GETs all the things -- run as root...";
-  exit 1; 
+  exit 1;
 fi
 
 echo "Update and upgrade all the things..."
 
 apt-get update -y
+
+apt-get dist-upgrade -y
 
 #install default ansible version and wget
 apt-get install ansible wget ca-certificates
@@ -18,18 +20,18 @@ update-ca-certificates
 #download bootstrap playbook
 wget -O ansible-bootstrap.yml https://raw.githubusercontent.com/akademiano-ansible/linux-bootstrap/master/ansible-bootstrap.yml
 
-ansible-playbook --connection=local 127.0.0.1 ansible-bootstrap.yml
+ansible-playbook ansible-bootstrap.yml
 
 wget -O ansible-roles-preload.yml https://raw.githubusercontent.com/akademiano-ansible/linux-bootstrap/master/ansible-roles-preload.yml
 
-ansible-playbook --connection=local 127.0.0.1 ansible-roles-preload.yml
+ansible-playbook ansible-roles-preload.yml
 
 #if not exist param with default roles-install playbook url - use akademiano-full
 
 ROLES_URL=https://gist.githubusercontent.com/mrdatamapper/75fbd48ab24c7e4509cc1ca6122a1d0d/raw/a84c025dd08f739371d1758f2c0994294e161661/akademiano-ansible-roles-all.yml
 
-wget -O bootstrap-roles.yml $ROLES_URL
+wget -O ansible-roles-install.yml $ROLES_URL
 
-ansible-playbook --connection=local 127.0.0.1 bootstrap-roles.yml
+ansible-playbook ansible-roles-install.yml
 
 exit 0;
