5323ac23e73812621bab9fc1884623f1e2d0ea9e
mrdatamapper <mrdatamapper@gmail.com>
2018-09-18 19:12:00 +0000

    small fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,19 @@ fi
 
 echo "Update and upgrade all the things..."
 
+service stop snapd
+
+mv /etc/apt/apt.conf.d/99update-notifier /root/99update-notifier
+
 apt-get update -y
 
 DEBIAN_FRONTEND=noninteractive \
+apt-get \
+-o Dpkg::Options::="--force-confnew" \
+-fuy \
+upgrade
 
+DEBIAN_FRONTEND=noninteractive \
 apt-get \
 -o Dpkg::Options::="--force-confnew" \
 -fuy \
@@ -26,13 +35,13 @@ update-ca-certificates
 wget -O ansible-bootstrap-1.yml https://raw.githubusercontent.com/akademiano-ansible/linux-bootstrap/master/ansible-bootstrap-1.yml
 wget -O ansible-bootstrap-2.yml https://raw.githubusercontent.com/akademiano-ansible/linux-bootstrap/master/ansible-bootstrap-2.yml
 
-ansible-playbook ansible-bootstrap-1.yml -v
+ansible-playbook ansible-bootstrap-1.yml -vv
 
-ansible-playbook ansible-bootstrap-2.yml
+ansible-playbook ansible-bootstrap-2.yml -vv
 
 wget -O ansible-roles-preload.yml https://raw.githubusercontent.com/akademiano-ansible/linux-bootstrap/master/ansible-roles-preload.yml
 
-ansible-playbook ansible-roles-preload.yml -v
+ansible-playbook ansible-roles-preload.yml -vv
 
 #if not exist param with default roles-install playbook url - use akademiano-full
 
@@ -40,6 +49,8 @@ ROLES_URL=https://gist.githubusercontent.com/mrdatamapper/75fbd48ab24c7e4509cc1c
 
 wget -O ansible-roles-install.yml $ROLES_URL
 
-ansible-playbook ansible-roles-install.yml -v
+ansible-playbook ansible-roles-install.yml -vv
+
+mv /root/99update-notifier /etc/apt/apt.conf.d/99update-notifier
 
 exit 0;
